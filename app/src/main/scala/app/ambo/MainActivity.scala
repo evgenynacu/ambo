2f602344609bd0ec36897b85c77d13d8b81d51d8
evgeny.nacu <evgeny.nacu@gmail.com>
2015-04-27 20:11:54 +0300

    preparing

--- app/src/main/scala/app/ambo/MainActivity.scala
+++ app/src/main/scala/app/ambo/MainActivity.scala
@@ -7,7 +7,7 @@ import android.support.v7.widget.{GridLayoutManager, RecyclerView}
 import android.view.ViewGroup
 import android.widget.TextView
 
-class MainActivity extends Activity{
+class MainActivity extends Activity {
 
   override def onCreate(savedInstanceState: Bundle): Unit = {
 
